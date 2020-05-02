4aba1f39e97ce0b53b4813cc67a2326bef9fb1b3
Javid Shaikh <javidshaikhf@gmail.com>
2020-05-02 19:07:11 +0530

    Initial commit

---
+++ Assets/UPWORK-SAPIZON/Scripts/InventoryItem.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InventoryItems
+{
+    Gun,
+    Axe
+
+}
+
+public class InventoryItem : MonoBehaviour
+{
+   
+    public bool isEntered;
+    public InventoryItems inventoryItemType;
+
+    public Transform inventoryPlaceHolderTransfrom;
+
+    public OVRGrabbable oVRGrabbable;
+
+    private void OnEnable() {
+        Debug.Log("Enabled");
+
+        oVRGrabbable.onGrabBegin += OnBeginGrab;
+        oVRGrabbable.onGrabEnd += OnBeginEnd;
+    }
+
+    private void OnDestroy() {
+        oVRGrabbable.onGrabBegin -= OnBeginGrab;
+         oVRGrabbable.onGrabEnd -= OnBeginEnd;
+    }
+
+    public void OnBeginGrab(){
+       
+    }
+
+    public void OnBeginEnd(){
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(gameObject.name +" Entering  "+ other.gameObject.name);
+
+        if(other.gameObject.tag.Equals("InventoryPlaceHolder")){
+            InventoryPlaceHolder inventoryPlaceHolder = other.gameObject.GetComponent<InventoryPlaceHolder>();
+            if(inventoryPlaceHolder && inventoryPlaceHolder.inventoryItemType ==  inventoryItemType){
+                isEntered = true;
+                inventoryPlaceHolderTransfrom = inventoryPlaceHolder.placeHolderTranform;
+                
+
+                SetPos();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log(gameObject.name +" Exiting  "+ other.gameObject.name);
+
+         if(other.gameObject.tag.Equals("InventoryPlaceHolder")){
+            InventoryPlaceHolder inventoryPlaceHolder = other.gameObject.GetComponent<InventoryPlaceHolder>();
+            if(inventoryPlaceHolder && inventoryPlaceHolder.inventoryItemType ==  inventoryItemType){
+                isEntered = false;
+            }
+        }
+    }
+
+
+    private void SetPos(){
+
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        transform.localPosition = inventoryPlaceHolderTransfrom.localPosition;
+        transform.rotation = inventoryPlaceHolderTransfrom.rotation;
+        transform.localScale = inventoryPlaceHolderTransfrom.localScale;
+    }
+
+}
---
+++ Assets/UPWORK-SAPIZON/Scripts/InventoryPlaceHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class InventoryPlaceHolder : MonoBehaviour
+{
+
+    public InventoryItems inventoryItemType;
+    
+    public Transform placeHolderTranform;
+
+    bool isEntered;
+
+    private void OnTriggerEnter(Collider other) {
+        isEntered = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        isEntered = false;
+    }
+
+}
